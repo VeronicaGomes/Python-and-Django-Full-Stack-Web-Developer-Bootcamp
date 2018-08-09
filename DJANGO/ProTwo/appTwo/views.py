4e84a35692d48c575ab4afb211b4ba1ff5407faa
joaossmacedo <joaossmacedo@gmail.com>
2018-08-09 17:34:01 -0300

    project solved DJANGO LEVEL TWO

--- DJANGO/ProTwo/appTwo/views.py
+++ DJANGO/ProTwo/appTwo/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from appTwo.models import User
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<em>My Second Project</em>");
+    return render(request, 'appTwo/index.html', context={})
 
-def help(request):
-    dictionary = {'insert' : 'HELP PAGE'}
-    return render(request, 'appTwo/help.html', context=dictionary)
+def users(request):
+    users_list = User.objects.order_by('first_name')
+    users_dict = {'users_info': users_list}
+
+    return render(request, 'appTwo/users.html', context=users_dict)
