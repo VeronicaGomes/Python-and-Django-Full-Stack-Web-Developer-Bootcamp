cd45be0bcf9292d3b18aea724abaead413646ff9
joaossmacedo <joaossmacedo@gmail.com>
2019-02-06 16:30:02 -0300

    solved on_delete missing, typo in length and wrong Field

--- DJANGO/FIRST_CLONE_PROJECT/mysite/blog/models.py
+++ DJANGO/FIRST_CLONE_PROJECT/mysite/blog/models.py
@@ -4,8 +4,8 @@ from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_lenght=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
     published_date = models.DateTimeField(blank=True, null=True)
@@ -24,8 +24,8 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post',related_name='comments')
-    author = models.ForeignKey(max_lenght=200)
+    post = models.ForeignKey('blog.Post',related_name='comments',on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
     approved_comment = models.BooleanField(default=False)
