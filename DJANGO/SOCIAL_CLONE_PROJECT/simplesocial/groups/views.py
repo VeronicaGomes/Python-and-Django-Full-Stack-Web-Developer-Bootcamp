7693d299db6ec7b40de221222d2fb302a0f96d68
joaossmacedo <joaossmacedo@gmail.com>
2019-02-15 19:02:05 -0300

    create CreateGroup, SingleGroup and ListGroups views

--- DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/groups/views.py
+++ DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/groups/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin,)
+from django.urls import reverse
+from django.views.generic import (CreateView, DetailView,
+                                  ListView,)
+from .models import (Group, GroupMember)
 
 # Create your views here.
+# Generic View(just used to copy and paste quicker)
+# class Generic(LoginRequiredMixin, View):
+#     model = Group
+
+class CreateGroup(CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+class SingleGroup(DetailView):
+    model = Group
+
+class ListGroups(ListView):
+    model = Group
