5d0189e806d160d8bcf128c0f79a85a035f810c2
joaossmacedo <joaossmacedo@gmail.com>
2019-02-16 18:04:10 -0300

    was using <username:pk> instead of <int:username>

--- DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/posts/urls.py
+++ DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/posts/urls.py
@@ -21,7 +21,7 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.PostList.as_view(), name='all'),
     path('new/', views.CreatePost.as_view(), name='create'),
-    path('by/<username:pk>/', views.UserPosts.as_view(), name='for_user'),
-    path('by/<username:pk>/<int:pk>/', views.PostDetail.as_view(), name='single'),
+    path('by/<int:username>/', views.UserPosts.as_view(), name='for_user'),
+    path('by/<int:usename>/<int:pk>/', views.PostDetail.as_view(), name='single'),
     path('delete/<int:pk>/', views.DeletePost.as_view(), name='delete'),
 ]
