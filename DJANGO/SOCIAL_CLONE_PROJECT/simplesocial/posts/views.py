908882a9c477256546193b5e2b614607d2b962ec
joaossmacedo <joaossmacedo@gmail.com>
2019-02-16 17:58:42 -0300

    line break was causing error in posts views

--- DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/posts/views.py
+++ DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/posts/views.py
@@ -24,8 +24,7 @@ class UserPosts(ListView):
 
     def get_queryset(self):
         try:
-            self.post.user = User.objects.prefetch_related('posts').
-                    get(username__iexact=self.kwargs.get('username'))
+            self.post.user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
         except User.DoesNotExist:
             raise Http404
         else:
