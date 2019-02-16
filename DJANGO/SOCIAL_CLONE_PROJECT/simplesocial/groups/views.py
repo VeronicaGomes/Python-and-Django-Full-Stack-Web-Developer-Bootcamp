ded5da8db52edbf1386b0efb1285729e135bc37e
joaossmacedo <joaossmacedo@gmail.com>
2019-02-16 17:29:43 -0300

    create JoinGroup view

--- DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/groups/views.py
+++ DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/groups/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         PermissionRequiredMixin,)
 from django.urls import reverse
+from django.shortcuts import get_object_or_404
 from django.views.generic import (CreateView, DetailView,
-                                  ListView,)
+                                  ListView, RedirectView)
 from .models import (Group, GroupMember)
 
 # Create your views here.
@@ -11,7 +13,7 @@ from .models import (Group, GroupMember)
 # class Generic(LoginRequiredMixin, View):
 #     model = Group
 
-class CreateGroup(CreateView):
+class CreateGroup(LoginRequiredMixin, CreateView):
     fields = ('name', 'description')
     model = Group
 
@@ -20,3 +22,21 @@ class SingleGroup(DetailView):
 
 class ListGroups(ListView):
     model = Group
+
+class JoinGroup(LoginRequiredMixin, RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+        try:
+            GroupMember.objects.create(user.self.request.user, group=group)
+        except IntegrityError:
+            messages.warning(self.request=,('Already a member!'))
+        else:
+            messages.warning(self.request=,('You are now a member of this group'))
+
+        return super().get(request, *args, **kwargs)
+
+class LeaveGroup(LoginRequiredMixin):
+    pass
