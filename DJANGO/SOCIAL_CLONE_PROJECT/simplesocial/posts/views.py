b0931c6ae9b5d9e0caa6404a0371e477f8884a84
joaossmacedo <joaossmacedo@gmail.com>
2019-02-16 17:59:06 -0300

    extra , was causing error in posts views

--- DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/posts/views.py
+++ DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin,
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.http import Http404
 from django.views.generic import (ListView, DetailView,
