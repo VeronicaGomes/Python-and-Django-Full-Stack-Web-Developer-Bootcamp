1f4500c129feddb6e52f54e1137e177dcae9a2da
joaossmacedo <joaossmacedo@gmail.com>
2019-02-16 17:37:05 -0300

    create join and leave group urls

--- DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/groups/urls.py
+++ DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/groups/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('', views.ListGroups.as_view(), name='all'),
     path('new/', views.CreateGroup.as_view(), name='create'),
     path('post/in/<slug:pk>/', views.SingleGroup.as_view(), name='single'),
+    path('join/<slug:pk>/', views.JoinGroup.as_view(), name='join'),
+    path('leave/<slug:pk>/', views.LeaveGroup.as_view(), name='leave'),
 ]
