36e7fe987846d8f20e87bfe5e64cfcb1c5a7d67c
joaossmacedo <joaossmacedo@gmail.com>
2019-02-18 20:32:02 -0300

    typed usename instead of username

--- DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/posts/urls.py
+++ DJANGO/SOCIAL_CLONE_PROJECT/simplesocial/posts/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='all'),
     path('new/', views.CreatePost.as_view(), name='create'),
     path('by/<str:username>/', views.UserPosts.as_view(), name='for_user'),
-    path('by/<str:usename>/<int:pk>/', views.PostDetail.as_view(), name='single'),
+    path('by/<str:username>/<int:pk>/', views.PostDetail.as_view(), name='single'),
     path('delete/<int:pk>/', views.DeletePost.as_view(), name='delete'),
 ]
